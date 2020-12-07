6dfbe9c9dad159820256349525b08da27237a708
mgu95 <mariuszgusc@outlook.com>
2020-12-07 01:04:20 +0100

    Initial commit

---
+++ src/main/java/config/DispatcherConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@ComponentScan(basePackages = "mvc")
+@EnableWebMvc
+public class DispatcherConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+    <head>
+        <title>Strona główna</title>
+    </head>
+    <body>
+        działa <3
+    </body>
+</html>
